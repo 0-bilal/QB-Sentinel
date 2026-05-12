e9def83b257984a32364077a3bd02ba1958b8376
0-bilal <140864157+0-bilal@users.noreply.github.com>
2026-05-12 12:22:56 +0300

    Update versions.js

--- js/versions.js
+++ js/versions.js
@@ -1,12 +1,12 @@
 const APP_VERSIONS = {
-    "ECL": "v1.4.0",
-    "FIL": "v1.3.10",
+    "ECL": "v1.4.1",
+    "FIL": "v1.4.0",
     "CPV": "v1.3.3",
     "ATT": "v1.1.0 BETA",
     "EDR": "BETA",
     "PRR": "BETA",
     "PAL": "BETA",
-    "GLOBAL": "v1.13.16" 
+    "GLOBAL": "v1.14.1" 
     //  2026/05/10 date
 };
 
